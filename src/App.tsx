af88f272e83d97adf6b1d2c924f34ce47d46a92d
kimdonggu42 <kdk3280@undermilli.com>
2024-05-20 21:02:02 +0900

    install eslint & prettier

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log('call');
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
     </View>
   );
 }
